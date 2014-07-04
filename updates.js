58748c9bbe111f3b353b94efae0a4f954bb4bbff
nats-ohchewy <dinosawrssayrawwr@gmail.com>
2014-07-03 18:38:11 -0700

    website updates using JSON file

---
+++ updates.js
@@ -0,0 +1,7 @@
+$document.getElementById("updates").innerhtml = function () {
+	$.getJSON ("https://spreadsheets.google.com/feeds/list/1THnq6q2JB0qNX_AQb5rBXc7H7Jrl3hNNbVu2GsHwdaY/od6/public/basic?hl=en_US&alt=json", function (obj) {
+		$.each (obj, function (key, value) {
+			$("ul").append("<li>+value.heading+</li>")
+		})
+	});
+}
