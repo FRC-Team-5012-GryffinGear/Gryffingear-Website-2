3ff06e7ce74973fe10f75e11f715dce37a233555
nats-ohchewy <dinosawrssayrawwr@gmail.com>
2014-07-07 16:51:03 -0700

    Updates

--- updates.js
+++
@@ -1,7 +0,0 @@
-$document.getElementById("updates").load function () {
-	$.getJSON ("https://spreadsheets.google.com/feeds/list/1THnq6q2JB0qNX_AQb5rBXc7H7Jrl3hNNbVu2GsHwdaY/od6/public/basic?hl=en_US&alt=json", function (obj) {
-		$.each (obj, function (key, value) {
-			$("ul").append("<li>+value.text+</li>")
-		})
-	});
-}
